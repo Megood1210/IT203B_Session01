628ffdf79b8acc23d224a852f3cc8c96764bebf2
Megood1210 <xuanvinhle06@gmail.com>
2026-03-09 08:45:50 +0700

    Upload bài tập 3

---
+++ Bai3.java
@@ -0,0 +1,26 @@
+class User {
+    private int age;
+    public void setAge(int age) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Tuổi không thể âm!");
+        }
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+public class Bai3 {
+    public static void main(String[] args) {
+        User user = new User();
+
+        try {
+            user.setAge(-1);
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
